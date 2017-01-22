f111257230ba182e47d5102410bccb56a63fab61
Krowk <lion.adam.otman@gmail.com>
2017-01-22 14:39:45 +0100

    Life bar debug

--- src/window/AwtManager.java
+++ src/window/AwtManager.java
@@ -233,9 +233,9 @@ public class AwtManager {
 						int t = (int)((character.getPosY()+character.getLongest()) * h/c.getHeight());
 						int u =((2*w/16));
 						int i = ((w/36));
-						g.drawRect((int)((character.getPosX() - c.getX()) * w/c.getWidth() +(w/2))+1 ,(int)((character.getPosY() - c.getY())* h/c.getHeight()+(h/2))-25, ((2*w/16)), ((w/36)));
+						g.drawRect((int)((character.getPosX() - c.getX()) * w/c.getWidth() +(w/2)) ,(int)((character.getPosY() - c.getY())* h/c.getHeight()+(h/2))-w/36, ((2*w/16)), ((w/36)));
 						g.setColor(Color.PINK);
-						g.fillRect((int)((character.getPosX() - c.getX()) * w/c.getWidth() +(w/2))+1,(int)((character.getPosY() - c.getY())* h/c.getHeight()+(h/2))-25 ,((int) ((double)(2*w/16)*l))-1, ((w/36)));
+						g.fillRect((int)((character.getPosX() - c.getX()) * w/c.getWidth() +(w/2)),(int)((character.getPosY() - c.getY())* h/c.getHeight()+(h/2))-w/36 ,((int) ((double)(2*w/16)*l))-1, ((w/36)));
 					}
 				}
 				
